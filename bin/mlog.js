44b64eca8625cedc6bec06cdcd2a899f93e781df
R32 <56782055@qq.com>
2021-12-20 01:50:33 +0800

    Updated mlog.js

--- bin/mlog.js
+++ bin/mlog.js
@@ -50,30 +50,6 @@ if (Object.defineProperty
   })();
 }
 
-/*!
-  Source: Eli Grey @ http://eligrey.com/blog/post/textcontent-in-ie8
-*/
-if (Object.defineProperty
-  && Object.getOwnPropertyDescriptor
-  && Object.getOwnPropertyDescriptor(Element.prototype, "textContent")
-  && !Object.getOwnPropertyDescriptor(Element.prototype, "textContent").get) {
-	(function() {
-		var innerText = Object.getOwnPropertyDescriptor(Element.prototype, "innerText");
-
-		Object.defineProperty(Element.prototype, "textContent",
-		  { // It won't work if you just drop in innerText.get
-			// and innerText.set or the whole descriptor.
-			get : function() {
-			  return innerText.get.call(this)
-			},
-			set : function(x) {
-			  return innerText.set.call(this, x)
-			}
-		  }
-		);
-	})();
-}
-
 /*! Date.now */
 if (!Date.now) {
 	Date.now = function now() { return new Date().getTime(); };
@@ -145,12 +121,7 @@ haxe_ValueException.prototype = $extend(haxe_Exception.prototype,{
 	}
 });
 var MLog = function() {
-	var tmp = dt.h("a",{ title : "close"},"x");
-	var tmp1 = dt.h("a",{ title : "clear output"},"c");
-	var tmp2 = dt.h("a",{ title : "refresh"},"r");
-	var tmp3 = dt.h("input",{ type : "text"});
-	var tmp4 = document.createElement("div");
-	this.ui = dt.h("div",{ id : "mlog"},[tmp,tmp1,tmp2,tmp3,tmp4]);
+	this.ui = __h("div",{ id : "mlog"},[__h("a",{ title : "close"},"x"),__h("a",{ title : "clear output"},"c"),__h("a",{ title : "refresh"},"r"),__h("input",{ type : "text"}),document.createElement("div")]);
 };
 MLog.attach = function(obj,type,handler) {
 	if(obj.addEventListener != null) {
@@ -220,7 +191,7 @@ MLog.onInputKeydown = function(e) {
 		} else if(value == "cls") {
 			MLog.mlog.clearOutput();
 		} else {
-			MLog.mlog.ui.children[4].appendChild(dt.h("li",{ 'class' : "err"},_g1));
+			MLog.mlog.ui.children[4].appendChild(__h("li",{ 'class' : "err"},_g1));
 			return;
 		}
 		MLog.mlog.ui.children[3].value = "";
@@ -260,7 +231,7 @@ MLog.log = function(v,infos) {
 		}
 		var node = MLog.mlog.logInner(label,v);
 		if(node != null) {
-			node.appendChild(dt.h("span",{ 'class' : "pos"},infos.fileName + ":" + infos.lineNumber));
+			node.appendChild(__h("span",{ 'class' : "pos"},infos.fileName + ":" + infos.lineNumber));
 			MLog.mlog.ui.children[4].appendChild(node);
 		}
 	}
@@ -268,7 +239,7 @@ MLog.log = function(v,infos) {
 MLog.injectCSS = function() {
 	var css = "#mlog{position:fixed;display:none;left:10%;right:10%;bottom:0;min-width:600px;border:#999999 1px solid;color:#2f363d;z-index:10;}#mlog>a{position:absolute;display:block;box-sizing:border-box;width:20px;height:20px;line-height:1;color:#666;cursor:pointer;background-color:transparent;border:1px solid #999999;font-family:consolas,monospace;font-size:16px;text-align:center;}#mlog>a:first-child{top:-20px;right:-1px;}#mlog>a[title^=clear]{top:29px;left:-20px;}#mlog>a[title=refresh]{top:-20px;right:18px;}#mlog>input[type=text]{display:block;width:100%;box-sizing:border-box;padding:4px 0;font-family:Arial,sans-serif;color:inherit;font-size:16px;outline:0;}#mlog>input[type=text]::-ms-clear{display:none;}#mlog>div{height:192px;font-family:consolas,monospace;background-color:#efefef;white-space:pre;overflow-y:auto;font-size:14px;list-style:none;}#mlog>div>pre{margin:0;font-family:consolas,monospace;color:#0366d6;}#mlog>div>li{margin:0;padding:0;border:1px #efefef solid;}#mlog>div>li pre{margin:0;padding-left:20px;}#mlog>div>li a{cursor:pointer;color:#0366d6;text-decoration:none;}#mlog>div>li.err{color:#f00;background-color:#ffeded;border-top-color:#da9393;border-bottom-color:#da9393;}#mlog>div span.ct{vertical-align:top;padding:1px 4px;margin-left:2px;background-color:#998fc7;color:#fff;font-size:12px;}#mlog>div span.pos{color:#666;float:right;text-align:right;padding:1px 2px 0 0;font-size:12px;}";
 	var tmp = window.document.querySelector("head");
-	var n = dt.h("style",{ type : "text/css"});
+	var n = __h("style",{ type : "text/css"});
 	if(n.styleSheet) {
 		n.styleSheet.cssText = css;
 	} else {
@@ -302,7 +273,7 @@ MLog.prototype = {
 	,usage: function() {
 		this.clearOutput();
 		var pre = document.createElement("pre");
-		pre.innerText = "Mini log[ver:" + "master de26269" + "] for IWebBrowser(Embeded IE)\r\ncls      : clear output\r\n$(\"s\")   : document.querySelector(\"s\")\r\n$" + "(\"s\")  : document.querySelectorAll(\"s\")\r\n";
+		pre.innerText = "Mini log[ver:" + "master fc06bf1" + "] for IWebBrowser(Embeded IE)\r\ncls      : clear output\r\n$(\"s\")   : document.querySelector(\"s\")\r\n$" + "(\"s\")  : document.querySelectorAll(\"s\")\r\n";
 		this.ui.children[4].appendChild(pre);
 	}
 	,parse: function(v,first) {
@@ -406,17 +377,17 @@ MLog.prototype = {
 		if(this.lines[0] != this.prev || this.ui.children[4].firstChild == null) {
 			this.prev = this.lines[0];
 			this.prevCount = 1;
-			return dt.h("li",null,this.prev);
+			return __h("li",null,this.prev);
 		}
 		var last = this.ui.children[4].lastChild;
 		++this.prevCount;
 		if(last.nodeType == 1) {
 			var conter = last.querySelector(".ct");
 			if(conter == null) {
-				conter = dt.h("span",{ 'class' : "ct"});
+				conter = __h("span",{ 'class' : "ct"});
 				last.appendChild(conter);
 			}
-			conter.textContent = "" + this.prevCount;
+			conter.innerText = "" + this.prevCount;
 		} else if(last.nodeType == 3) {
 			last.nodeValue = Std.string(this.prev) + "  " + "(" + this.prevCount + ")";
 		}
@@ -424,7 +395,7 @@ MLog.prototype = {
 	}
 	,multiple: function(expr) {
 		this.prev = null;
-		return dt.h("li",null,[dt.h("a",null,expr),dt.h("pre",null,this.lines.join("\n"))]);
+		return __h("li",null,[__h("a",null,expr),__h("pre",null,this.lines.join("\n"))]);
 	}
 	,logInner: function(expr,v) {
 		this.lines = [];
@@ -443,32 +414,31 @@ MLog.prototype = {
 		}
 	}
 };
-var dt = function() { };
-dt.h = function(name,attr,sub) {
-	var dom = window.document.createElement(name);
+function __h(name,attr,sub) {
+	var dom = document.createElement(name);
 	if(attr != null) {
 		for(var k in attr) dom.setAttribute(k, attr[k]);
 	}
 	if(sub) {
-		dt.hrec(dom,sub,false);
+		__hrec(dom,sub,false);
 	}
 	return dom;
-};
-dt.hrec = function(box,sub,loop) {
+}
+function __hrec(box,sub,loop) {
 	if(((sub) instanceof Array)) {
 		var i = 0;
 		var len = sub.length;
 		while(i < len) {
-			dt.hrec(box,sub[i],true);
+			__hrec(box,sub[i],true);
 			++i;
 		}
 	} else if(typeof(sub) == "object") {
 		box.appendChild(sub);
 	} else if(loop) {
-		box.appendChild(window.document.createTextNode(sub));
+		box.appendChild(document.createTextNode(sub));
 	} else {
-		box.textContent = sub;
+		box.innerText = sub;
 	}
-};
+}
 MLog.main();
 })(typeof console != "undefined" ? console : {log:function(){}}, {});
