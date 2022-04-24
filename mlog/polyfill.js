eba094ba3eea6b392dec4e9e60d41bd55083f076
R32 <56782055@qq.com>
2022-04-24 21:05:44 +0800

    fixed

--- mlog/polyfill.js
+++ mlog/polyfill.js
@@ -7,12 +7,14 @@ if (window.console == null) {
 			var slice = Array.prototype.slice;
 			var args = slice.call(arguments, 0);
 			var pinfo = null;
-			var p = args[0].split(":");
-			if (p.length >= 3) {
-				args = slice.call(args, 1);
-				if (args.length == 0)
-					args = [slice.call(p, 2).join(":")];
-				pinfo = {fileName: p[0], lineNumber: p[1] | 0};
+			if (typeof(args[0]) == "string") {
+				var p = args[0].split(":");
+				if (p.length >= 3) {
+					args = slice.call(args, 1);
+					if (args.length == 0)
+						args = [slice.call(p, 2).join(":")];
+					pinfo = {fileName: p[0], lineNumber: p[1] | 0};
+				}
 			}
 			MLog.log(args.length == 1 ? args[0] : args, pinfo);
 		}
